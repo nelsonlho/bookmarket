915377c85a3deb4b7368ba028af76c4a01127ceb
Nelson Ho <nelsonlho@gmail.com>
2016-11-16 00:52:35 -0800

    added sale model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,11 +1,15 @@
 class Book < ApplicationRecord
   extend FriendlyId
 	friendly_id :name, use: :slugged
-  
+
   belongs_to :user
+	has_many :sales
 
   has_attached_file :image
 	has_attached_file :resource
+
+  validates_numericality_of :price,
+		greather_then: 49, message: "Must be at least 50 cents"
 
   validates_attachment_content_type :image,
 		content_type:  /^image\/(png|gif|jpeg)/,
